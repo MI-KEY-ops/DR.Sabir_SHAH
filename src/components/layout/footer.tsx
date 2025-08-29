867eb4463059aebc9f99a720ecc3481471442cfe
Haris Miraj <harismirajse@gmail.com>
2025-08-29 15:02:48 +0000

    ( 2025 Vitae. All Rights Reserved) to ( 2025 VDEV All Rights Reserved.)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-secondary text-secondary-foreground py-6">
       <div className="container mx-auto text-center text-sm">
-        <p>&copy; {new Date().getFullYear()} Vitae. All Rights Reserved.</p>
+        <p>&copy; {new Date().getFullYear()} VDEV. All Rights Reserved.</p>
         <p className="mt-1">Designed for Dr. Sabir Shah</p>
       </div>
     </footer>
